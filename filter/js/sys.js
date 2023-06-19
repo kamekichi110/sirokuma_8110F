ed0d460d9ee8ba52d2912278be5e9e8c56cd5ace
kamekichi110 <123218419+kamekichi110@users.noreply.github.com>
2023-06-20 08:03:24 +0900

    delete

--- filter/js/sys.js
+++
@@ -1,8 +0,0 @@
-var element = document.getElementById("viewer");
-
-element.addEventListener("load", function(event) {
-  let url = document.getElementById("viewer").contentWindow.location.href
-  if (url = includes('youtube.com')) {
-    viewer.location = "error.html"
-  } //true
-});
