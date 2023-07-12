77de013566b8fe41e01ae15501adafabbee7c280
kamekichi110 <123218419+kamekichi110@users.noreply.github.com>
2023-07-13 07:23:51 +0900

    d

--- public/js/script00.js
+++
@@ -1,8 +0,0 @@
-function menuOpen() {
-  var menu = document.getElementById("menu");
-  menu.style.display = "block";
-}
-function menuClose() {
-  var menu = document.getElementById("menu");
-  menu.style.display = "none";
-}
