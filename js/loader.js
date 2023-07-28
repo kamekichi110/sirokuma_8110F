18107debb058a3f1b90f4e6e0def8555b3f33cbc
kamekichi110 <123218419+kamekichi110@users.noreply.github.com>
2023-07-28 14:16:20 +0900

    uy

--- js/loader.js
+++ js/loader.js
@@ -2,4 +2,6 @@ window.onload = window.setTimeout(
 function load() {
     var loader = document.getElementById("loader");
     loader.style.display = "none";
-},5000);
+    var html = document.getElementById("html");
+    html.style.display = "block";
+},3000);
