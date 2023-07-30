31308437299f65999af16e8dff8388bcb68e35fd
kamekichi110 <123218419+kamekichi110@users.noreply.github.com>
2023-07-30 10:15:04 +0900

    gg

---
+++ voice/tobu8000.js
@@ -0,0 +1,58 @@
+function SETdef() {
+  var img = document.getElementById("image");
+  var def = "./lib/def.png";
+  img.src = def;
+}
+function SEThappy() {
+  var img = document.getElementById("image");
+  var happy = "./lib/happy.png";
+  img.src = happy;
+}
+function SETangry() {
+  var img = document.getElementById("image");
+  var angry = "./lib/angry.png";
+  img.src = angry;
+}
+function SETdeath() {
+  var img = document.getElementById("image");
+  var death = "./lib/death.png";
+  img.src = death;
+}
+function SETguruguru() {
+  var img = document.getElementById("image");
+  var guruguru = "./lib/guruguru.png";
+  img.src = guruguru;
+}
+function SEThou() {
+  var img = document.getElementById("image");
+  var hou = "./lib/hou.png";
+  img.src = hou;
+}
+function SETodoroku() {
+  var img = document.getElementById("image");
+  var odoroku = "./lib/odoroku.png";
+  img.src = odoroku;
+}
+function SETpokan() {
+  var img = document.getElementById("image");
+  var pokan = "./lib/pokan.png";
+  img.src = pokan;
+}
+function SETsleep() {
+  var img = document.getElementById("image");
+  var sleep = "./lib/sleep.png";
+  img.src = sleep;
+}
+function smile() {
+  var img = document.getElementById("image");
+  var smile = "./lib/smile.png";
+  img.src = smile;
+}
+function reset() {
+  SETdef();
+}
+function hello() {
+  SEThappy();
+  var music000 = document.getElementById("music000");
+  music000.play();
+}
