6a4f3a0f5098141f0eb297d34d45e944d900ec03
kamekichi110 <123218419+kamekichi110@users.noreply.github.com>
2023-07-31 15:45:14 +0900

    er

--- chat/index.js
+++ chat/index.js
@@ -6,6 +6,12 @@
   var guruguru = "./img/guruguru.png";
   var sound = document.getElementById("sound");
   function preview() {
+    var text = document.getElementById("input").value;
+    var result = document.getElementById("output");
+    var image = document.getElementById("preview");
+    var def = "./img/def.png";
+    var happy = "./img/happy.png";
+    var sound = document.getElementById("sound");
   result.innerHTML = text;
   image.src = happy;
   image.alt = "happy";
@@ -19,6 +25,12 @@
   }, 5200);
 }
 function preview_guruguru() {
+  var text = document.getElementById("input").value;
+  var result = document.getElementById("output");
+  var image = document.getElementById("preview");
+  var def = "./img/def.png";
+  var guruguru = "./img/guruguru.png";
+  var sound = document.getElementById("sound");
   result.innerHTML = text;
   image.src = guruguru;
   image.alt = "guruguru";
