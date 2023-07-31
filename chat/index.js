0802d44dc074f8974fbf6f44109fca0151b3121e
kamekichi110 <123218419+kamekichi110@users.noreply.github.com>
2023-07-31 15:34:07 +0900

    tiny

--- chat/index.js
+++ chat/index.js
@@ -1,13 +1,27 @@
-function preview() {
   var text = document.getElementById("input").value;
   var result = document.getElementById("output");
   var image = document.getElementById("preview");
   var def = "./img/def.png";
   var happy = "./img/happy.png";
+  var guruguru = "./img/guruguru.png";
   var sound = document.getElementById("sound");
+  function preview() {
   result.innerHTML = text;
   image.src = happy;
   image.alt = "happy";
+  setTimeout(() => {
+    sound.play();
+  }, 250);
+  setTimeout(() => {
+    image.src = def;
+    image.alt = "def";
+    result.innerHTML = "";
+  }, 5200);
+}
+function preview_guruguru() {
+  result.innerHTML = text;
+  image.src = guruguru;
+  image.alt = "guruguru";
   setTimeout(() => {
     sound.play();
   }, 250);
