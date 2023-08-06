8aad80540ba8171bfcbe056ca885dc8f82109c02
kamekichi110 <123218419+kamekichi110@users.noreply.github.com>
2023-08-06 09:13:23 +0900

    rty

--- chat/entity/index.js
+++ chat/entity/index.js
@@ -1,5 +1,38 @@
 function happy() {
     var img = document.getElementById("entity");
     var happy = "./asset/entity-happy.png";
-    var input = document.getElementById("input")
+    var input = document.getElementById("input");
+    var text = document.getElementById("text");
+    var select = document.getElementById("list").value;
+    var sound = document.getElementById(select);
+    var def = "./asset/entity-def.png";
+    var blank = "";
+    img.src = happy;
+    text.innerHTML = input.value;
+    sound.play();
+    setTimeout(() => {
+        img.src = def;
+        input.value = blank;
+        text.innerHTML = blank;
+        input.focus();
+    }, 4200);
+}
+function guruguru() {
+    var img = document.getElementById("entity");
+    var guruguru = "./asset/entity-guruguru.png";
+    var def = "./asset/entity-def.png";
+    var input = document.getElementById("input");
+    var output = document.getElementById("text");
+    var select = document.getElementById("list").value;
+    var sound = document.getElementById(select);
+    var blank = "";
+    img.src = guruguru;
+    output.innerHTML = input.value;
+    sound.play();
+    setTimeout(() => {
+        img.src = def;
+        output.innerHTML = blank;
+        input.value = blank;
+        input.focus();
+    }, 4200);
 }
