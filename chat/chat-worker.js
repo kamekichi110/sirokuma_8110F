c03200067fda4c56a07c7f092b688712741aa4d4
kamekichi110 <123218419+kamekichi110@users.noreply.github.com>
2023-08-09 19:52:50 +0900

    rt

---
+++ chat/chat-worker.js
@@ -0,0 +1,8 @@
+var img = document.getElementById("preview");
+var output = document.getElementById("output");
+var input = document.getElementById("input");
+var list = document.getElementById("list").value;
+var sound = document.getElementById(list);
+function SE() {
+    sound.play();
+}
