1c050f75e7e309bc9841a293c2562a6c8cbc5cfb
kamekichi110 <123218419+kamekichi110@users.noreply.github.com>
2023-08-11 23:26:39 +0900

    BGM

--- chat/video.js
+++ chat/video.js
@@ -1,19 +1,9 @@
-function start() {
-    setInterval(() => {
-html2canvas(document.getElementById("bg"), {
-  // オプション
-  width: 320, // 幅を設定
-  height: 180, // 高さを設定
-  windowWidth: 320, // html2canvas の幅
-}).then(function (canvas) {
-  document.getElementById("result").src = canvas.toDataURL();
-});
-    },500);
-    var canvas = document.getElementById("result");
-    const stream = canvas.captureStream();
-    recorder = new MediaRecorder(stream, { mimeType: "video/webm;codecs=vp9" });
-    recorder.start();
+function BGM_start() {
+  var BGM = document.getElementById("BGM");
+  BGM.play();
 }
-function stop() {
-    recorder.stop();
+function BGM_stop() {
+  var BGM = document.getElementById("BGM");
+  BGM.pause();
+  BGM.currentTime = 0;
 }
