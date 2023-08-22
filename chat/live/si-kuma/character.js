f1b2efb0bc948d627741826153e05225ce022ea6
kamekichi110 <123218419+kamekichi110@users.noreply.github.com>
2023-08-23 07:59:14 +0900

    er

---
+++ chat/live/si-kuma/character.js
@@ -0,0 +1,30 @@
+function def() {
+    var entity = document.getElementById("entity");
+    var def = "./asset/def.PNG";
+    entity.src = def;
+    entity.alt = "default";
+}
+function smile() {
+    var entity = document.getElementById("entity");
+    var smile = "./asset/smile.PNG";
+    entity.src = smile;
+    entity.alt = "smile";
+}
+function mogu() {
+    var entity = document.getElementById("entity");
+    var mogu = "./asset/mogumogu.PNG";
+    entity.src = mogu;
+    entity.alt = "mogumogu";
+}
+function hekomu() {
+    var entity = document.getElementById("entity");
+    var hekomu = "./asset/hekomu.PNG";
+    entity.src = hekomu;
+    entity.alt = "hekomu";
+}
+function ho() {
+    var entity = document.getElementById("entity");
+    var ho = "./asset/ho.PNG";
+    entity.src = ho;
+    entity.alt = "hou";
+}
