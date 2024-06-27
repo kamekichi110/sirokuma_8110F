6c60fc46e76979a8323b1af4c1ad0c8462f8e03f
kamekichi110 <123218419+kamekichi110@users.noreply.github.com>
2024-06-27 21:28:51 +0900

    set

---
+++ home/js/json.js
@@ -0,0 +1,12 @@
+function loadJson(url) {
+    return fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+      });
+  }
