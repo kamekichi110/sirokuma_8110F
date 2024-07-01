fb754ce9ae25b9858d104eb21f45e58b687d12d7
kamekichi110 <123218419+kamekichi110@users.noreply.github.com>
2024-07-01 09:58:59 +0900

    set

--- 3ds/led/include/html-header.php
+++ 3ds/led/include/html-header.php
@@ -2,4 +2,4 @@
     <meta name="description" content="3DSブラウザ対応！鉄道車両LED表示器再現ツール">
     <meta name="keywords" content="3ds,3DS,LED,led">
     <meta name="viewport" content="width=320">
-    <title>LED表示器再現ツール for 3DS</title>
+    <title>英単語 for 3DS</title>
