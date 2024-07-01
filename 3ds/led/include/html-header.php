8fe327199211bf42831d0839cb05f374a046ade0
kamekichi110 <123218419+kamekichi110@users.noreply.github.com>
2024-07-01 09:15:15 +0900

    set

---
+++ 3ds/led/include/html-header.php
@@ -0,0 +1,5 @@
+    <meta charset="UTF-8">
+    <meta name="description" content="3DSブラウザ対応！鉄道車両LED表示器再現ツール">
+    <meta name="keywords" content="3ds,3DS,LED,led">
+    <meta name="viewport" content="width=320">
+    <title>LED表示器再現ツール for 3DS</title>
---
+++ 3ds/led/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <?php
+    include './include/html-header.php'
+    ?>
+    <link rel="stylesheet" href="./css/main.css">
+    <link rel="shortcut icon" href="./icons/favicon.ico" type="image/x-icon">
+    <link rel="manifest" href="app.json">
+</head>
+<body>
+    
+</body>
+</html>
