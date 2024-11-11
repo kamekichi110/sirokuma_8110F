5ef71d14fc41830a85a576182df78ec1bbb11dbc
kamekichi110 <123218419+kamekichi110@users.noreply.github.com>
2024-11-11 18:12:10 +0900

    Create screen.js

---
+++ TOHO/screen.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  var OUT = document.getElementById('canvas');
+  OUT.style.margin = "0px auto";
+  OUT.style.width "100%";
+  OUT.style.aspectRatio = "5 / 4";
+});
