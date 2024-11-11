441fc243d5f0751c46b5f3a22aba51f45f00bbed
kamekichi110 <123218419+kamekichi110@users.noreply.github.com>
2024-11-11 11:27:32 +0900

    Add files via upload

---
+++ TOHO/script.js
@@ -0,0 +1,16 @@
+const boot = document.getElementById("boot");
+const msg = document.getElementById("msg");
+const cmd = document.getElementById("cmd");
+const bat = document.getElementById("bat");
+
+document.addEventListener("DOMContentLoaded", function () {
+ setTimeout(() => {
+  msg.style.display = "block";
+  setTimeout(() => {
+   cmd.style.display = "block";
+   setTimeout(() => {
+    bat.style.display = "block";
+   }, 1280);
+  }, 1000);
+ }, 500);
+});
