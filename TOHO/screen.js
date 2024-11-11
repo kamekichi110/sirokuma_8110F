fd462bf4ff609264422d83dd3fde0eab126337b6
kamekichi110 <123218419+kamekichi110@users.noreply.github.com>
2024-11-11 18:12:56 +0900

    Update screen.js

--- TOHO/screen.js
+++ TOHO/screen.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
   var OUT = document.getElementById('canvas');
   OUT.style.margin = "0px auto";
   OUT.style.width "100%";
   OUT.style.aspectRatio = "5 / 4";
+  },1280);
 });
